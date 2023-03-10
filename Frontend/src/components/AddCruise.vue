d83831355ad0f04cf8b7e814d5e33260d30aa901
hassannh <hassannouhi91@gmail.com>
2023-03-10 17:31:03 +0100

    fix add function

--- Frontend/src/components/AddCruise.vue
+++ Frontend/src/components/AddCruise.vue
@@ -4,30 +4,30 @@
 <div class="p-8 rounded border border-gray-200">
     <h1 class="font-medium text-3xl">Add cruise</h1>
     <!-- <p class="text-gray-600 mt-6">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos dolorem vel cupiditate laudantium dicta.</p> -->
-    <form method="POST" action="<?= URLROOT ?>cruiseController/add_cruise">
+    <form @submit.prevent="Submit">
         <div class="mt-8 grid lg:grid-cols-2 gap-4">
             <div>
                 <label for="name" class="text-sm text-gray-700 block mb-1 font-medium">Name</label>
-                <input type="text" name="name" id="name" class="bg-gray-100 border border-gray-200 rounded py-1 px-3 block focus:ring-blue-500 focus:border-blue-500 text-gray-700 w-full" placeholder="Cruise name" required/>
+                <input type="text" v-model="form.name" id="name" class="bg-gray-100 border border-gray-200 rounded py-1 px-3 block focus:ring-blue-500 focus:border-blue-500 text-gray-700 w-full" placeholder="Cruise name" required/>
             </div>
             <div>
                 <label for="price" class="text-sm text-gray-700 block mb-1 font-medium">Price</label>
-                <input type="number" name="price" class="bg-gray-100 border border-gray-200 rounded py-1 px-3 block focus:ring-blue-500 focus:border-blue-500 text-gray-700 w-full" placeholder="$ OR £ OR DH" required/>
+                <input type="number" v-model="form.price" class="bg-gray-100 border border-gray-200 rounded py-1 px-3 block focus:ring-blue-500 focus:border-blue-500 text-gray-700 w-full" placeholder="$ OR £ OR DH" required/>
             </div>
             <div>
                 <label for="picture" class="text-sm text-gray-700 block mb-1 font-medium">picture</label>
-                <input type="file" name="picture" class="bg-gray-100 border border-gray-200 rounded py-1 px-3 block focus:ring-blue-500 focus:border-blue-500 text-gray-700 w-full" placeholder="picture" required/>
+                <input type="file" class="bg-gray-100 border border-gray-200 rounded py-1 px-3 block focus:ring-blue-500 focus:border-blue-500 text-gray-700 w-full"  placeholder="picture" required/>
             </div>
             <div>
                 <label for="Date" class="text-sm text-gray-700 block mb-1 font-medium">Date</label>
-                <input type="date" name="Date" id="date_reservation" class="bg-gray-100 border border-gray-200 rounded py-1 px-3 block focus:ring-blue-500 focus:border-blue-500 text-gray-700 w-full" placeholder="Date" required/>
+                <input type="date" v-model="form.Date" id="date_reservation" class="bg-gray-100 border border-gray-200 rounded py-1 px-3 block focus:ring-blue-500 focus:border-blue-500 text-gray-700 w-full" placeholder="Date" required/>
             </div>
             <div>
                 <label for="nights" class="text-sm text-gray-700 block mb-1 font-medium">Nights</label> <input type="number" name="nights" class="bg-gray-100 border border-gray-200 rounded py-1 px-3 block focus:ring-blue-500 focus:border-blue-500 text-gray-700 w-full" placeholder="nights number" required/>
             </div>
             <div>
                 <label for="brithday" class="text-sm text-gray-700 block mb-1 font-medium">Ship</label>
-                <select class="block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded-lg h-10 px-4 md:w-full " required name="ship">
+                <select v-model="form.ship" class="block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded-lg h-10 px-4 md:w-full " required name="ship">
                     <option value="" disabled selected>select ship</option>
 
 
@@ -45,7 +45,7 @@
 
             <div>
                 <label for="brithday" class="text-sm text-gray-700 block mb-1 font-medium">Port</label>
-                <select class="block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded-lg h-10 px-4 md:w-full " required name="ports">
+                <select v-model="form.ports" class="block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded-lg h-10 px-4 md:w-full " required name="ports">
                     <option value="" disabled selected>select port</option>
                     <!-- <?php foreach ($data['port'] as $port) : ?> -->
                         <option value="<?= $port->id ?>">
@@ -60,7 +60,7 @@
                 <label class="form-label">Escale a:</label><br>
                 <div class="container">
                     <div class="row">
-                        <input type="text" name="trajet" id="traget" class="bg-gray-100 border border-gray-200 rounded py-1 px-3 block focus:ring-blue-500 focus:border-blue-500 text-gray-700 w-full" required >
+                        <input type="text" v-model="form.trajet" id="traget" class="bg-gray-100 border border-gray-200 rounded py-1 px-3 block focus:ring-blue-500 focus:border-blue-500 text-gray-700 w-full" required >
 
                         <div class="col-md-6">
                             <select id="select_traget" multiple>
@@ -87,3 +87,40 @@
     </form>
 </div>
 </template>
+
+
+<script>
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      form: {
+        name : "",
+            ship :"" ,
+            price :"",
+            picture : "",
+            nights : "",
+            ports : "",
+            Date : "",
+            trajet : ""
+      },
+    }
+  },
+  methods: {
+ 
+    async Submit() {
+      try {
+        axios.post('http://localhost/Fill_Rouge/backend/cruiseController/add_cruise', JSON.stringify(this.form))
+        .then(res => {
+          localStorage.setItem("id_user", JSON.stringify( res.data.user.id ));
+        })
+        
+      this.$router.push({ path: "/dashboard" });
+      } catch (error) {
+        console.error(error)
+      }
+    },
+  
+}
+}
+</script>
